7314d0abb67a0bca30a88b0e3c260581b88408f2
Mohammad Zaif Quraishi <mzaifquraishi@users.noreply.github.com>
2021-08-02 21:00:22 +0530

    second commit

--- data.js
+++ data.js
@@ -1,66 +1,851 @@
 var json = [{
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/100wonTahun.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10paisaback.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10paisafront.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10paise1963.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10paise1985.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupee.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupeeCoronacoin.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupeeDrBRAmbedkar.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupeeGandhi.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupeeHomiBhabha.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupeeLines.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
+        img: "coins/10rupeeRBI.jpg",
+        title: "",
+        desc: "",
     },
     {
-        "img": "coin/coin.jpg",
-        "title": "TITLE",
-        "desc": ""
-    }
-]
+        img: "coins/10rupeeSymbol.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/10rupeeWithSymbol.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/10rupeeYogo.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1dimeBack.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1NayaPaisa1963.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1paisa1944.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1pice1951.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1976.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1981.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1984.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1984_.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1985.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1985_.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1985__.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1985___.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1989.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1993.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1997.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1998.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee1999.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupee2017.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeCellularJailCoin.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeFamilyCOin.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeJawaharlalNehru.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeLokNayakJayaPrakesh.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeRajeevGandhi.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeSwamiDnyaneshwar.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/1rupeeSwamiVivekananda.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/20paise1970.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/20paise1985.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/25paise1984back.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/25paise1984front.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/25paise1985.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2NepaliBack.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2NepaliFront.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2pen1971.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupee1995.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupee2008.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupee2009.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupee8thWorldTamilConference.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeBOI.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeDelhiOlympic.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeDesbhanduChitaranhan.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeIXAsianGames1982.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeLouisBraille.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeSardarVallabbhaiPatel.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeSardarVallabhaiPatel_.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeSubhashChandraBose.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeSupreemCourtOfIndia_.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeSupremeCourtOfIndia.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/2rupeeWaterForEveryone.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50paise1973.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50paise1985.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50paise1988.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50paise1994.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50paise2008.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50paiseDandiMarch.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/50reyaal.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5paise1968.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee2000.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee2009.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee2011.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee2019Lines.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee50thAnivOfUN.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupee8thWorldTamilConference.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeBegumAkhtar.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeBhagatSingh.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeBhagwanMahavir.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeBHEL.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeC.Subramaniam.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeCivilAviationIndia.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeConcilOfMedicalResearch.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeConstitutionOfIndia.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeDadabhaiNaoroji.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeDandiMarch.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeFoodAndAgricultureOrg.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeK.kamaraj.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeLalBahadurShastri.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeMaulanaAbulKalamAzad.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeMintKolkata.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeMotherTeresa.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeMotiLalNehru.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeONGC.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeParlementOfIndia.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeRabindraNathTagoor.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeRajendraPrasad.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeRajendraPrasad-56.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeRBI.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeSBI.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeShreeNarayanGur.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeTheFirstWarOfIndependence.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeVaishnoDevi.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/5rupeeWorldOfWork.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinscoin.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_004018.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_004020.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005241.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005245.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005245_11.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005245_13.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005245_16.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005245_44.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005820.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_005821.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_010515.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_010516.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_010518.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_010518_12.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_011248.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_011249.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_011734.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_011814.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_012209.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_012211.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_012750.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_012751.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_013525.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_013946.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014045.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014224.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014244.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014432.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014545.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014620.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014649.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014724.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_014855.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015040.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015150.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015454.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015615.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015702.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015737.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015812.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015840.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_015918.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020031.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020207.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020215.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020242.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020318.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020421.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020440.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020559.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinsIMG_20210802_020606.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coinslibertyfront.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/riyall.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_64.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_76.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_78.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_80.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_83.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_85.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_86.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_88.jpg",
+        title: "",
+        desc: "",
+    },
+    {
+        img: "coins/t_89.jpg",
+        title: "",
+        desc: "",
+    },
+];
